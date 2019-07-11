54c89f1788bc1d946447b5d510db19c46aae402e
Jan Verweij <jan.verweij@liferay.com>
2019-07-11 10:29:10 +0200

    added some basic methods

--- src/main/java/com/liferay/demo/commerce/payment/AdyenCommercePaymentMethod.java
+++ src/main/java/com/liferay/demo/commerce/payment/AdyenCommercePaymentMethod.java
@@ -1,8 +1,12 @@
 package com.liferay.demo.commerce.payment;
 
-import CommercePaymentMethod;
 
+import com.liferay.commerce.payment.method.CommercePaymentMethod;
+import com.liferay.commerce.payment.request.CommercePaymentRequest;
+import com.liferay.commerce.payment.result.CommercePaymentResult;
 import org.osgi.service.component.annotations.Component;
+
+import java.util.Locale;
 
 /**
  * @author jverweij
@@ -15,6 +19,46 @@ import org.osgi.service.component.annotations.Component;
 	service = CommercePaymentMethod.class
 )
 public class AdyenCommercePaymentMethod implements CommercePaymentMethod {
+	
+	@Override
+	public CommercePaymentResult completePayment(CommercePaymentRequest commercePaymentRequest) throws Exception {
+		return null;
+	}
+
+	@Override
+	public String getDescription(Locale locale) {
+		return null;
+	}
+
+	@Override
+	public String getKey() {
+		return null;
+	}
+
+	@Override
+	public String getName(Locale locale) {
+		return null;
+	}
+
+	@Override
+	public int getPaymentType() {
+		return 0;
+	}
+
+	@Override
+	public String getServletPath() {
+		return null;
+	}
+
+	@Override
+	public boolean isCompleteEnabled() {
+		return false;
+	}
+
+	@Override
+	public boolean isProcessPaymentEnabled() {
+		return false;
+	}
 
 	// TODO enter required service methods
 
