9387b71ebd58ba1096672650149c6a4b0dfea1bf
Jan Verweij <jan.verweij@liferay.com>
2019-07-12 09:24:21 +0200

    added language resources

--- src/main/java/com/liferay/demo/commerce/payment/AdyenCommercePaymentMethod.java
+++ src/main/java/com/liferay/demo/commerce/payment/AdyenCommercePaymentMethod.java
@@ -1,12 +1,18 @@
 package com.liferay.demo.commerce.payment;
 
 
+import com.liferay.commerce.constants.CommerceOrderConstants;
+import com.liferay.commerce.constants.CommercePaymentConstants;
 import com.liferay.commerce.payment.method.CommercePaymentMethod;
 import com.liferay.commerce.payment.request.CommercePaymentRequest;
 import com.liferay.commerce.payment.result.CommercePaymentResult;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import org.osgi.service.component.annotations.Component;
 
+import java.util.Collections;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 /**
  * @author jverweij
@@ -19,47 +25,68 @@ import java.util.Locale;
 	service = CommercePaymentMethod.class
 )
 public class AdyenCommercePaymentMethod implements CommercePaymentMethod {
-	
+
+	public static final String KEY = "adyen-payment-method";
+
 	@Override
 	public CommercePaymentResult completePayment(CommercePaymentRequest commercePaymentRequest) throws Exception {
-		return null;
+		return new CommercePaymentResult(
+				null, commercePaymentRequest.getCommerceOrderId(),
+				CommerceOrderConstants.PAYMENT_STATUS_PAID, false, null, null,
+				Collections.emptyList(), true);
 	}
 
 	@Override
 	public String getDescription(Locale locale) {
-		return null;
+		ResourceBundle resourceBundle = _getResourceBundle(locale);
+
+		return LanguageUtil.get(resourceBundle, "");
 	}
 
 	@Override
 	public String getKey() {
-		return null;
+		return KEY;
 	}
 
 	@Override
 	public String getName(Locale locale) {
-		return null;
+		ResourceBundle resourceBundle = _getResourceBundle(locale);
+
+		return LanguageUtil.get(resourceBundle, KEY);
 	}
 
 	@Override
 	public int getPaymentType() {
-		return 0;
+		return CommercePaymentConstants.COMMERCE_PAYMENT_METHOD_TYPE_ONLINE_REDIRECT;
 	}
 
 	@Override
 	public String getServletPath() {
+		// is used to identify the servlet that manages the payment. It is required by the interface but does nothing in this case.
 		return null;
 	}
 
 	@Override
 	public boolean isCompleteEnabled() {
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isProcessPaymentEnabled() {
-		return false;
+		return true;
 	}
 
-	// TODO enter required service methods
+	@Override
+	public CommercePaymentResult processPayment(CommercePaymentRequest commercePaymentRequest) throws Exception {
+		return new CommercePaymentResult(
+				null, commercePaymentRequest.getCommerceOrderId(),
+				CommerceOrderConstants.PAYMENT_STATUS_AUTHORIZED, false, null, null,
+				Collections.emptyList(), true);
+	}
+
+	private ResourceBundle _getResourceBundle(Locale locale) {
+		return ResourceBundleUtil.getBundle(
+				"content.Language", locale, getClass());
+	}
 
 }
